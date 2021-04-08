3e5ec10fc09c4ed151b889652c82a2c411233b2b
Yaoyuan-Z <76600279+Yaoyuan-Z@users.noreply.github.com>
2021-04-07 21:55:15 -0400

    Update JumpGame.java
    add solution 2 here

--- src/JumpGame.java
+++ src/JumpGame.java
@@ -9,3 +9,24 @@ class Solution {
         return lastPosition ==0;
     }
     }
+
+// solution 2
+class Solution {
+    public boolean canJump(int[] nums) {
+        int start = 0;
+        int end = nums[0];
+        while (end < nums.length-1){
+            int max = end;
+            for(int i = start;i<=end; i++){
+                max = Math.max(max,i+nums[i]);
+            }
+            if(start >= end){
+                return false;
+            }
+            start = end;
+            end = max;
+        }
+        return true;
+    }
+    
+}
